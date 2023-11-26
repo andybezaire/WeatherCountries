529509935fe15ad6a0d0da26eaf86c5f000f5dda
andy <andy.bezaire@nordea.com>
2023-11-26 19:14:37 +0200

    Initial Commit

---
+++ WeatherCountries/WeatherCountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCountriesApp.swift
+//  WeatherCountries
+//
+//  Created by andy on 26.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
